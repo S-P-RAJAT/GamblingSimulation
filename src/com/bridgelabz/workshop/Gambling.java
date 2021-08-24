d276395be0f786a6416902fc6de43dc02b78d15b
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-24 09:09:40 +0530

    ADDED: Stake increase or decrease based on win/lose status

--- src/com/bridgelabz/workshop/Gambling.java
+++ src/com/bridgelabz/workshop/Gambling.java
@@ -1,14 +1,26 @@
 package com.bridgelabz.workshop;
 
 public class Gambling {
-	static final int stake = 100;
-	static final int dayBettingPrice = 1;
+	public static final int STAKE = 100;
+	public static int GAMBLER_STAKE = 100;
+	public static final int DAY_BETTING_PRICE = 1;
+	public void play() {
+		
+		if((Math.random()*100)>50) {
+			System.out.println("Win");
+			GAMBLER_STAKE += 1;
+		}else {
+			System.out.println("Lose");
+			GAMBLER_STAKE -= 1;
+
+		}
+	}
 
 public static void main(String[] args) {
 	System.out.println("Welcome to Gambling Simulation!");
 	Gambling gambler = new Gambling();
-	System.out.println("Stake:"+ gambler.stake);
-	System.out.println("Per day betting price:"+ gambler.dayBettingPrice);
+	System.out.println("Stake:"+ gambler.STAKE);
+	System.out.println("Per day betting price:"+ gambler.DAY_BETTING_PRICE);
 
 }
 }
