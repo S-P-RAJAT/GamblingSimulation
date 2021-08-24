1ff02174daefc9a766d3a1b65e757e155d739287
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-24 08:57:50 +0530

    REFACTOR: Stake and Betting details added

--- src/com/bridgelabz/workshop/Gambling.java
+++ src/com/bridgelabz/workshop/Gambling.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.workshop;
 
 public class Gambling {
+	static final int stake = 100;
+	static final int dayBettingPrice = 1;
 
 public static void main(String[] args) {
 	System.out.println("Welcome to Gambling Simulation!");
+	Gambling gambler = new Gambling();
+	System.out.println("Stake:"+ gambler.stake);
+	System.out.println("Per day betting price:"+ gambler.dayBettingPrice);
 
 }
 }
