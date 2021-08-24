7abde81beca1823e497c6b4d5344ddd1c9ff01b0
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-24 09:15:02 +0530

    ADDED: Terminating conditions of game

--- src/com/bridgelabz/workshop/Gambling.java
+++ src/com/bridgelabz/workshop/Gambling.java
@@ -4,23 +4,29 @@ public class Gambling {
 	public static final int STAKE = 100;
 	public static int GAMBLER_STAKE = 100;
 	public static final int DAY_BETTING_PRICE = 1;
-	public void play() {
-		
-		if((Math.random()*100)>50) {
-			System.out.println("Win");
+
+	public boolean play() {
+
+		if ((Math.random() * 100) > 50) {
+			System.out.println("Win: " + GAMBLER_STAKE);
 			GAMBLER_STAKE += 1;
-		}else {
-			System.out.println("Lose");
+		} else {
+			System.out.println("Lose: " + GAMBLER_STAKE);
 			GAMBLER_STAKE -= 1;
 
 		}
+		if (GAMBLER_STAKE > 150 || GAMBLER_STAKE < 50)
+			return false;
+		else
+			return true;
 	}
 
-public static void main(String[] args) {
-	System.out.println("Welcome to Gambling Simulation!");
-	Gambling gambler = new Gambling();
-	System.out.println("Stake:"+ gambler.STAKE);
-	System.out.println("Per day betting price:"+ gambler.DAY_BETTING_PRICE);
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulation!");
+		Gambling gambler = new Gambling();
+		System.out.println("Stake:" + gambler.STAKE);
+		System.out.println("Per day betting price:" + gambler.DAY_BETTING_PRICE);
+		while (gambler.play());
 
-}
+	}
 }
